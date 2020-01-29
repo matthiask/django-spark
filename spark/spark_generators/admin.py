9377e20b69be257ff31737c97f4479319cd9eb8c
Matthias Kestenholz <mk@feinheit.ch>
2020-01-29 09:30:11 +0100

    Replace ugettext* with gettext*

--- spark/spark_generators/admin.py
+++ spark/spark_generators/admin.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib import admin
 from django.utils.html import format_html, format_html_join
 from django.utils.text import capfirst
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .models import Condition, Generator
 
--- spark/spark_mails/admin.py
+++ spark/spark_mails/admin.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils.module_loading import import_string
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .models import Mail
 
